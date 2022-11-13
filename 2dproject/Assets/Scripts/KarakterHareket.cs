64976516bb71147bb992dc0ca888ebb0d6b9da78
ezikbebe <erenyanar84@gmail.com>
2022-11-13 21:12:27 +0300

    Scripts Demo

--- 2dproject/Assets/Scripts/KarakterHareket.cs
+++ 2dproject/Assets/Scripts/KarakterHareket.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
-{
-    // Start is called before the first frame update
+public class KarakterHareket: MonoBehaviour
+{   //Burda karaktere verdiðimiz özelliklere referans veriyoruz ki kod üstünde rahat kullanalým
+    Rigidbody2D rgb;
+    Vector3 velocity;
+    
+
+    //verdiðimiz hareketlerin deðerlerini ayarlýyoruz
+    float speedAmount = 5f;
+    float jumpAmount = 3f;
+
+
     void Start()
     {
-        
+        //Burda rgb elde ediyoruz
+        rgb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        //yürüme ayarlarý
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0f);
+        transform.position += velocity * speedAmount * Time.deltaTime;
         
+        //zýplama ayarý
+        if (Input.GetButtonDown("Jump") && Mathf.Approximately(rgb.velocity.y, 0))
+
+        {
+            rgb.AddForce(Vector3.up * jumpAmount, ForceMode2D.Impulse);
+           
+        }
+        
+        //karakterin kamera bakýs acýsýný deðiþtirme
+        if (Input.GetAxisRaw("Horizontal") == -1)
+        {
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+        }
+        else if (Input.GetAxisRaw("Horizontal") == 1)
+        {
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        }
     }
+
+
 }
